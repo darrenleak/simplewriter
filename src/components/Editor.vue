bd1b45145ad83f75c733eeec32d92432fad18658
Darren <dleak22@gmail.com>
2025-07-02 10:29:56 +0200

    Initial

---
+++ src/components/Editor.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { Marked } from 'marked'
+import { ref } from 'vue'
+
+const markdown = ref('')
+const parsed = ref('')
+
+function textInputChanged() {
+  parsed.value = new Marked().parse(markdown.value)
+  console.log('Hello there this is a test')
+}
+</script>
+<template>
+  <div class="container">
+    <textarea class="textArea" @keyup="textInputChanged" v-model="markdown" autofocus></textarea>
+    <div class="parsedMarkdown" v-html="parsed"></div>
+  </div>
+</template>
+
+<style>
+.container {
+  display: flex;
+}
+
+.textArea {
+  height: 100vh;
+  flex-grow: 1;
+  border: 0;
+  outline: none;
+}
+
+.parsedMarkdown {
+  height: 100vh;
+  overflow-y: auto;
+  flex-grow: 1;
+}
+</style>
